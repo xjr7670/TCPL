5b4321d5b583aa8a05edc43b5556a4cd182969a7
Cavin <xjr30226@126.com>
2018-07-31 22:25:47 +0800

    exercise1-9

---
+++ ch1/exercise1-9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* exercise1-9: write a program to copy its input to its output, 
+ * replacing each string of one or more blanks by a single blank.
+ */
+
+void main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {}
+    }
+}
